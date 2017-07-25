408fc152a9a2346e08f83c9a245924346f353323
Glenn Galvizo <glennjgalvizo@gmail.com>
2017-07-24 17:34:58 -1000

    Chomp is now a child class of Nibble.

--- test/storage/chomp/test-chomp.cpp
+++ test/storage/chomp/test-chomp.cpp
@@ -11,9 +11,11 @@
  * against the k-vector query time.
  */
 void TestChomp::test_regular_query() {
-    SQLite::Database db(Nibble::database_location, SQLite::OPEN_READWRITE | SQLite::OPEN_CREATE);
+    Nibble nb;
     std::vector<double> kaph;
-    kaph = Nibble::search_table(db, "SEP20", "theta BETWEEN 5.004 and 5.005", "theta", 90, 30);
+
+    nb.select_table("SEP20");
+    kaph = nb.search_table("theta BETWEEN 5.004 and 5.005", "theta", 90, 30);
 
     for (unsigned int a = 0; a < kaph.size(); a++) {
         std::string test_name = "RegularQueryResultWithinBoundsSet" + std::to_string(a + 1);
@@ -25,19 +27,17 @@ void TestChomp::test_regular_query() {
  * Check that the k-vector query returns the correct results.
  */
 void TestChomp::test_k_vector_query() {
-    SQLite::Database db(Nibble::database_location, SQLite::OPEN_READWRITE | SQLite::OPEN_CREATE);
+    Chomp ch;
     std::vector<double> kaph;
-    kaph = Chomp::k_vector_query(db, "SEP20", "theta", "theta", 5.004, 5.005, 90);
+
+    ch.select_table("SEP20");
+    kaph = ch.k_vector_query("theta", "theta", 5.004, 5.004, 90);
 
     for (unsigned int a = 0; a < kaph.size(); a++) {
         std::string test_name = "KVectorQueryResultWithinBoundsSet" + std::to_string(a + 1);
         assert_true(kaph[a] > 5.003 && kaph[a] < 5.006, test_name);
     }
 }
-
-/*
- * Check that
- */
 
 /*
  * Enumerate all tests in TestChomp.
@@ -60,12 +60,5 @@ int TestChomp::enumerate_tests(int test_case) {
  * Run the tests in TestChomp.
  */
 int main() {
-//    SQLite::Database db(Nibble::database_location, SQLite::OPEN_READWRITE | SQLite::OPEN_CREATE);
-//    SQLite::Transaction t(db);
-//    db.exec("DROP TABLE SEP20_KVEC");
-//    t.commit();
-//    Chomp::create_k_vector("SEP20", "theta");
-//    auto a = Nibble::search_table(db, "SEP20", "theta BETWEEN ")
-//    return 0;
     return TestChomp().execute_tests();
 }
