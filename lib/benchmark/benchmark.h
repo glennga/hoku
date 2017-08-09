98c7bf95105e19234fe61bd63e5c97f980139af1
Glenn Galvizo <glennjgalvizo@gmail.com>
2017-08-09 12:13:47 -1000

    Using new doxygen comments. Using new style guide.

--- lib/benchmark/benchmark.h
+++ lib/benchmark/benchmark.h
@@ -1,9 +1,7 @@
-/*
- * @file: benchmark.h
- *
- * @brief: Header file for Benchmark class, which generates the input data for star
- * identification testing.
- */
+/// @file benchmark.h
+/// @author Glenn Galvizo
+///
+/// Header file for Benchmark class, which generates the input data for star identification testing.
 
 #ifndef HOKU_BENCHMARK_H
 #define HOKU_BENCHMARK_H
@@ -16,76 +14,76 @@
 #include "rotation.h"
 #include "nibble.h"
 
-/*
- * @class Benchmark
- * @brief Benchmark class, which generates the input data for star identification testing.
- *
- * The environment variable HOKU_PROJECT_PATH must point to top level of this project.
- * The following Python script must exist: %HOKU_PROJECT_PATH%/lib/benchmark/generate_plot.py
- *
- * The benchmark class is used for all star identification implementation testing. To imitate
- * real data from a star detector, we search for all stars in a section of the sky and apply
- * various error models to this set.
- */
-class Benchmark {
-        friend class TestBenchmark;
-        friend class TestAngle;
-        friend class TestPlanarTriangle;
+/// The benchmark class is used for all star identification implementation testing. To imitate real data from a star
+/// detector, we search for all stars in a section of the sky and apply various error models to this set.
+///
+/// The environment variable HOKU_PROJECT_PATH must point to top level of this project.
+/// The following Python script must exist: %HOKU_PROJECT_PATH%/lib/benchmark/generate_plot.py
+class Benchmark
+{
+private:
+    friend class TestBenchmark;
+    friend class TestAngle;
+    friend class TestPlanarTriangle;
 
-    public:
-        // error model structure, used to define the type of error and the stars affected.
-        struct ErrorModel {
-            std::string model_name;
-            std::string plot_color;
-            std::vector<Star> affected;
-        };
+public:
+    /// Error model structure. Defines the type of error and the stars affected.
+    struct ErrorModel
+    {
+        std::string model_name; /// Name of the error being applied.
+        std::string plot_color; /// MatPlotLib color to label the affected points.
+        Star::list affected;    /// List of stars affected by the error.
+    };
 
-        // default constructor must not be generated, user must specify fov, focus, and rotation
-        Benchmark() = delete;
-        Benchmark(const double, const Star &, const Rotation & = Rotation::identity());
+    /// Default constructor must not be generated. User must specify fov, focus, and rotation.
+    Benchmark() = delete;
 
-        // find all stars that are near the focus
-        void generate_stars();
+    Benchmark(const double, const Star &, const Rotation & = Rotation::identity());
 
-        // set stars, focus, and fov from parameters
-        void present_image(Star::list &, Star &, double &);
+    void generate_stars();
 
-        // write current star set to file, display plot using Python's MatPlotLib
-        int record_current_plot();
-        int display_plot();
+    void present_image(Star::list &, Star &, double &);
 
-        // error models: stray light, blocked light, shifted stars
-        void add_extra_light(const int);
-        void remove_light(const int, const double);
-        void shift_light(const int, const double);
+    int record_current_plot();
+    int display_plot();
 
-    private:
-        using model_list = std::vector<ErrorModel>;
+    void add_extra_light(const int);
+    void remove_light(const int, const double);
+    void shift_light(const int, const double);
 
-        // set all of the BCS IDs in the star set to 0 and return the current star set
-        Star::list clean_stars();
+private:
+    /// Alias for the list (stack) of ErrorModels.
+    using model_list = std::vector<ErrorModel>;
 
-        // shuffle current star set
-        void shuffle();
+    Star::list clean_stars();
+    void shuffle();
 
-        // location of plot files, requires definition of HOKU_PROJECT_PATH
-        const std::string PROJECT_LOCATION = std::string(std::getenv("HOKU_PROJECT_PATH"));
-        const std::string CURRENT_PLOT = PROJECT_LOCATION + "/data/cuplt.tmp";
-        const std::string ERROR_PLOT = PROJECT_LOCATION + "/data/errplt.tmp";
-        const std::string PLOT_SCRIPT = PROJECT_LOCATION + "/lib/benchmark/generate_plot.py";
+    /// String of the HOKU_PROJECT_PATH enviroment variable
+    const std::string PROJECT_LOCATION = std::string(std::getenv("HOKU_PROJECT_PATH"));
 
-        // all stars in 'stars' must be near the focus
-        Star::list stars;
-        Star focus;
+    /// Path of the 'clean' star set on disk. One of the temporary files used for plotting.
+    const std::string CURRENT_PLOT = PROJECT_LOCATION + "/data/cuplt.tmp";
 
-        // limit a star must be from focus
-        double fov;
+    /// Path of the 'error' star set on disk. One of the temporary files used for plotting.
+    const std::string ERROR_PLOT = PROJECT_LOCATION + "/data/errplt.tmp";
 
-        // rotation applied to all stars, moves from inertial to image
-        Rotation inertial_to_image;
+    /// Path of the Python script used to plot the current Benchmark instance.
+    const std::string PLOT_SCRIPT = PROJECT_LOCATION + "/lib/benchmark/generate-plot.py";
 
-        // error models, all changed stars
-        model_list error_models;
+    /// Current list of stars. All stars must be near the focus.
+    Star::list stars;
+
+    /// The focal point of the star list. Does not necessarily have to be a BSC5 star itself.
+    Star focus;
+
+    /// All stars must be fov degrees from the focus.
+    double fov;
+
+    /// Rotation applied to all stars. Moves stars from inertial to image.
+    Rotation inertial_to_image;
+
+    /// List (stack) of ErrorModels, which also holds all changed stars.
+    model_list error_models;
 };
 
 #endif /* HOKU_BENCHMARK_H */
