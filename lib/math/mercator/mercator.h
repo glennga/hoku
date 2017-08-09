2ed49b71aadb51ff101327893c3c5b7f970bfc7e
Glenn Galvizo <glennjgalvizo@gmail.com>
2017-08-08 16:14:02 -1000

    Using doxygen comments. Using new code style.

--- lib/math/mercator/mercator.h
+++ lib/math/mercator/mercator.h
@@ -1,61 +1,60 @@
-/*
- * @file: mercator.h
- *
- * @brief: Header file for Mercator class, which represents two-dimensional projections of three-
- * dimensional unit vectors (Stars).
- */
+/// @file mercator.h
+/// @author Glenn Galvizo
+///
+/// Header file for Mercator class, which represents two-dimensional projections of three-dimensional unit vectors
+/// (i.e. Stars).
 
 #ifndef HOKU_MERCATOR_H
 #define HOKU_MERCATOR_H
 
 #include "star.h"
 
-/*
- * @class Mercator
- * @brief Mercator class, which represents a Star in two dimensions.
- *
- * The mercator class is meant to reduce a dimension off of a star. This class is used to
- * flatten the points in the BSC5 table and as the main data in the BSC5 quadtree.
- */
-class Mercator {
-        friend class TestMercator;
-        friend class TestChomp;
+/// The mercator class is meant to reduce a dimension off of a star. This class is used to flatten the points in
+/// the BSC5 table and as the main data in the BSC5 quadtree.
+class Mercator
+{
+    friend class TestMercator;
+    friend class TestChomp;
 
-    public:
-        using list = std::vector<Mercator>;
-        using quad = std::array<Mercator, 4>;
+public:
+    /// Alias for a vector of Mercator points.
+    using list = std::vector<Mercator>;
 
-        Mercator() = default;
+    /// Alias for a quartet of Mercator points.
+    using quad = std::array<Mercator, 4>;
 
-        // constructors given a star, or a set of coordinates
-        Mercator(const Star &, const double);
-        Mercator(const double, const double, const double, const int = 0);
+    /// Force default constructor. Default point is (0, 0) with w_n = 0 and hr = 0.
+    Mercator() = default;
 
-        // given a list of points, reduce to only the closest points to current
-        Mercator::list reduce_far_points(const Mercator::list &, const double);
+    Mercator(const Star &, const double);
+    Mercator(const double, const double, const double, const int = 0);
 
-        // access method coordinates, width, and hr
-        void present_projection(double &, double &, double &, int &) const;
+    Mercator::list reduce_far_points(const Mercator::list &, const double);
 
-        // another access method for hr only
-        int get_hr();
+    void present_projection(double &, double &, double &, int &) const;
 
-    protected:
-        // find the corners of a box, given size 'a'
-        Mercator::quad find_corners(const double) const;
+    int get_hr();
 
-        // check if a point is within a defined quadrilateral
-        bool is_within_bounds(const quad &) const;
+protected:
+    Mercator::quad find_corners(const double) const;
 
-        // coordinates and projection width
-        double x = 0, y = 0, w_n = 0;
+    bool is_within_bounds(const quad &) const;
 
-        // Harvard revised number
-        int hr = 0;
+protected:
+    /// X coordinate of the projected point. Default point is (0, 0).
+    double x = 0;
 
-    private:
-        // project 3D vector to plane
-        void project_star(const Star &, const double);
+    /// Y coordinate of the projected point. Default point is (0, 0).
+    double y = 0;
+
+    /// Width of the map the point is projected onto. Default width is 0.
+    double w_n = 0;
+
+    /// Harvard Revised number for the point. Default is 0.
+    int hr = 0;
+
+private:
+    void project_star(const Star &, const double);
 };
 
 #endif /* HOKU_MERCATOR_H */
