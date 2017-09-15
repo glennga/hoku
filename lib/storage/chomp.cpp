220d584ae5acfbd15f98a65e20d99316212e4561
Glenn Galvizo <glennjgalvizo@gmail.com>
2017-09-15 13:40:58 -1000

    K-vector generation is no longer O(n^2)! Now ~ O(n)!

--- lib/storage/chomp.cpp
+++ lib/storage/chomp.cpp
@@ -25,10 +25,19 @@ int Chomp::build_k_vector_table (const std::string &focus_column, const double m
     select_table(table + "_KVEC");
     
     // Load K-Vector into table, K(i) = j where s(j) <= z(i) < s(j + 1).
+    double k_value_hat = 0, j_hat = 0;
     for (int i = 0; i < s_l; i++) {
-	double k_value = 0;
-        for (int j = 0; j < s_l; j = (s_vector[j] < ((m * i) + q)) ? j + 1 : s_l) {
-	    k_value += (s_vector[j] < ((m * i) + q)) ? 1 : 0;
+	double k_value = k_value_hat;
+        for (int j = j_hat; j < s_l; j++) {
+	    if (s_vector[j] < m * i + q) {
+	        k_value++;
+	    }
+	    else {
+                // We remember our previous point, and continue here for i + 1.
+		k_value_hat = k_value;
+		j_hat = j;
+		break;
+	    }
 	}
 
         insert_into_table("k_value", tuple {k_value});
