7e4ed7409e5ebcd25f071fde99af66fb463926c7
Glenn Galvizo <glennjgalvizo@gmail.com>
2017-12-07 13:27:34 -1000

    Now shuffling the inertial set in the same order as the body.

--- lib/trial/semi-crown.cpp
+++ lib/trial/semi-crown.cpp
@@ -21,15 +21,18 @@
 /// @param q Reference to where the actual rotation of the image will be placed.
 void SemiCrown::present_benchmark (Chomp &ch, std::random_device &seed, Star::list &body, Star::list &inertial,
                                    Star &focus, const double m_bar, Rotation &q) {
-    std::mt19937_64 mersenne_twister(seed());
+    std::mt19937_64 mt1(seed());
+    auto mt2 = mt1;
+
+    std::vector<int> indices;
     q = Rotation::chance(seed);
-    
+
     // We require at-least five stars to exist here.
     do {
         focus = Star::chance(seed);
         body.clear(), body.reserve((unsigned int) WORKING_FOV * 4);
         inertial.clear(); inertial.reserve((unsigned int) WORKING_FOV * 4);
-        
+
         for (const Star &s : ch.nearby_hip_stars(focus, WORKING_FOV / 2.0, (unsigned int) WORKING_FOV * 4)) {
             if (s.get_magnitude() < m_bar) {
                 inertial.emplace_back(s);
@@ -38,9 +41,10 @@ void SemiCrown::present_benchmark (Chomp &ch, std::random_device &seed, Star::li
         }
     }
     while (body.size() < 5);
-    
+
     // Shuffle and rotate the focus.
-    std::shuffle(body.begin(), body.end(), mersenne_twister);
+    std::shuffle(body.begin(), body.end(), mt1);
+    std::shuffle(inertial.begin(), inertial.end(), mt2);
     focus = Rotation::rotate(focus, q);
 }
 
@@ -54,14 +58,14 @@ void SemiCrown::trial_angle (Chomp &ch, std::ofstream &log) {
     Rotation q, q_actual;
     Star::list body, inertial;
     Star focus;
-    
+
     // These are the optimal parameters for the Angle method.
     Angle::Parameters par;
     par.table_name = ANGLE_TABLE;
     par.z_max = 20000;
     par.match_sigma = std::numeric_limits<double>::epsilon() * pow(3, 5);
     par.query_sigma = std::numeric_limits<double>::epsilon() * pow(3, 5);
-    
+
     // First run is clean, without shifts. Following are the shift trials.
     for (int ss_i = 0; ss_i < SS_ITER; ss_i++) {
         for (int mb_i = 0; mb_i < MB_ITER; mb_i++) {
@@ -69,20 +73,20 @@ void SemiCrown::trial_angle (Chomp &ch, std::ofstream &log) {
                 for (int i = 0; i < CROWN_SAMPLES; i++) {
                     present_benchmark(ch, seed, body, inertial, focus, MB_MIN + mb_i * MB_STEP, q_actual);
                     Benchmark input(seed, body, focus, WORKING_FOV);
-                    
+
                     // Append our error.
                     input.shift_light((int) input.stars.size(), ((ss_i == 0) ? 0 : SS_MULT * pow(10, ss_i)));
                     double p = ES_MIN + ES_STEP * es_i, clean_size = input.stars.size();
                     input.add_extra_light((int) ((p / (1 - p)) * clean_size));
-                    
+
                     // Find the resulting rotation.
                     z = 0;
                     q = Angle::trial_semi_crown(input, par, z);
-                    
+
                     // Log our results.
                     log << "Angle," << par.match_sigma << "," << par.query_sigma << ","
                         << ((ss_i == 0) ? 0 : SS_MULT * pow(10, ss_i)) << "," << MB_MIN + mb_i * MB_STEP << "," << p
-                        << "," << z << "," << Rotation::rotation_difference(q_actual, q, inertial.back()).norm()
+                        << "," << z << "," << Rotation::rotation_difference(q_actual, q, inertial[0]).norm()
                         << "\n";
                 }
             }
@@ -100,7 +104,7 @@ void SemiCrown::trial_plane (Chomp &ch, std::ofstream &log) {
     Rotation q, q_actual;
     Star::list body, inertial;
     Star focus;
-    
+
     // These are the optimal parameters for the Plane method.
     Plane::Parameters par;
     par.table_name = PLANE_TABLE;
@@ -108,7 +112,7 @@ void SemiCrown::trial_plane (Chomp &ch, std::ofstream &log) {
     par.match_sigma = std::numeric_limits<double>::epsilon() * pow(3, 5);
     par.sigma_a = std::numeric_limits<double>::epsilon() * pow(3, 5);
     par.sigma_i = std::numeric_limits<double>::epsilon() * pow(3, 5);
-    
+
     // First run is clean, without shifts. Following are the shift trials.
     for (int ss_i = 0; ss_i < SS_ITER; ss_i++) {
         for (int mb_i = 0; mb_i < MB_ITER; mb_i++) {
@@ -116,20 +120,20 @@ void SemiCrown::trial_plane (Chomp &ch, std::ofstream &log) {
                 for (int i = 0; i < CROWN_SAMPLES; i++) {
                     present_benchmark(ch, seed, body, inertial, focus, MB_MIN + mb_i * MB_STEP, q_actual);
                     Benchmark input(seed, body, focus, WORKING_FOV);
-                    
+
                     // Append our error.
                     input.shift_light((int) input.stars.size(), ((ss_i == 0) ? 0 : SS_MULT * pow(10, ss_i)));
                     double p = ES_MIN + ES_STEP * es_i, clean_size = input.stars.size();
                     input.add_extra_light((int) ((p / (1 - p)) * clean_size));
-                    
+
                     // Find our result set. Run the identification.
                     z = 0;
                     q = Plane(input, par).trial_semi_crown(z);
-                    
+
                     // Log our results.
                     log << "Plane," << par.match_sigma << "," << par.sigma_a << ","
                         << ((ss_i == 0) ? 0 : SS_MULT * pow(10, ss_i)) << "," << MB_MIN + mb_i * MB_STEP << "," << p
-                        << "," << z << "," << Rotation::rotation_difference(q_actual, q, inertial.back()).norm()
+                        << "," << z << "," << Rotation::rotation_difference(q_actual, q, inertial[0]).norm()
                         << "\n";
                 }
             }
@@ -147,7 +151,7 @@ void SemiCrown::trial_sphere (Chomp &ch, std::ofstream &log) {
     Rotation q, q_actual;
     Star::list body, inertial;
     Star focus;
-    
+
     // These are the optimal parameters for the Sphere method.
     Sphere::Parameters par;
     par.table_name = SPHERE_TABLE;
@@ -155,7 +159,7 @@ void SemiCrown::trial_sphere (Chomp &ch, std::ofstream &log) {
     par.match_sigma = std::numeric_limits<double>::epsilon() * pow(3, 5);
     par.sigma_a = std::numeric_limits<double>::epsilon() * pow(3, 5);
     par.sigma_i = std::numeric_limits<double>::epsilon() * pow(3, 5);
-    
+
     // First run is clean, without shifts. Following are the shift trials.
     for (int ss_i = 0; ss_i < SS_ITER; ss_i++) {
         for (int mb_i = 0; mb_i < MB_ITER; mb_i++) {
@@ -163,20 +167,20 @@ void SemiCrown::trial_sphere (Chomp &ch, std::ofstream &log) {
                 for (int i = 0; i < CROWN_SAMPLES; i++) {
                     present_benchmark(ch, seed, body, inertial, focus, MB_MIN + mb_i * MB_STEP, q_actual);
                     Benchmark input(seed, body, focus, WORKING_FOV);
-                    
+
                     // Append our error.
                     input.shift_light((int) input.stars.size(), ((ss_i == 0) ? 0 : SS_MULT * pow(10, ss_i)));
                     double p = ES_MIN + ES_STEP * es_i, clean_size = input.stars.size();
                     input.add_extra_light((int) ((p / (1 - p)) * clean_size));
-                    
+
                     // Find our result set. Run the identification.
                     z = 0;
                     q = Sphere(input, par).trial_semi_crown(z);
-                    
+
                     // Log our results.
                     log << "Sphere," << par.match_sigma << "," << par.sigma_a << ","
                         << ((ss_i == 0) ? 0 : SS_MULT * pow(10, ss_i)) << "," << MB_MIN + mb_i * MB_STEP << "," << p
-                        << "," << z << "," << Rotation::rotation_difference(q_actual, q, inertial.back()).norm()
+                        << "," << z << "," << Rotation::rotation_difference(q_actual, q, inertial[0]).norm()
                         << "\n";
                 }
             }
@@ -194,14 +198,14 @@ void SemiCrown::trial_pyramid (Chomp &ch, std::ofstream &log) {
     Rotation q, q_actual;
     Star::list body, inertial;
     Star focus;
-    
+
     // These are the optimal parameters for the Pyramid method.
     Pyramid::Parameters par;
     par.table_name = PYRAMID_TABLE;
     par.z_max = 20000;
     par.match_sigma = std::numeric_limits<double>::epsilon() * pow(3, 5);
     par.query_sigma = std::numeric_limits<double>::epsilon() * pow(3, 5);
-    
+
     // First run is clean, without shifts. Following are the shift trials.
     for (int ss_i = 0; ss_i < SS_ITER; ss_i++) {
         for (int mb_i = 0; mb_i < MB_ITER; mb_i++) {
@@ -209,20 +213,20 @@ void SemiCrown::trial_pyramid (Chomp &ch, std::ofstream &log) {
                 for (int i = 0; i < CROWN_SAMPLES; i++) {
                     present_benchmark(ch, seed, body, inertial, focus, MB_MIN + mb_i * MB_STEP, q_actual);
                     Benchmark input(seed, body, focus, WORKING_FOV);
-                    
+
                     // Append our error.
                     input.shift_light((int) input.stars.size(), ((ss_i == 0) ? 0 : SS_MULT * pow(10, ss_i)));
                     double p = ES_MIN + ES_STEP * es_i, clean_size = input.stars.size();
                     input.add_extra_light((int) ((p / (1 - p)) * clean_size));
-                    
+
                     // Find our result set. Run the identification.
                     z = 0;
                     q = Pyramid::trial_semi_crown(input, par, z);
-                    
+
                     // Log our results.
                     log << "Pyramid," << par.match_sigma << "," << par.query_sigma << ","
                         << ((ss_i == 0) ? 0 : SS_MULT * pow(10, ss_i)) << "," << MB_MIN + mb_i * MB_STEP << "," << p
-                        << "," << z << "," << Rotation::rotation_difference(q_actual, q, inertial.back()).norm()
+                        << "," << z << "," << Rotation::rotation_difference(q_actual, q, inertial[0]).norm()
                         << "\n";
                 }
             }
@@ -240,7 +244,7 @@ void SemiCrown::trial_coin (Chomp &ch, std::ofstream &log) {
     Rotation q, q_actual;
     Star::list body, inertial;
     Star focus;
-    
+
     // These are the optimal parameters for the Coin method.
     Coin::Parameters par;
     par.table_name = COIN_TABLE;
@@ -248,7 +252,7 @@ void SemiCrown::trial_coin (Chomp &ch, std::ofstream &log) {
     par.match_sigma = std::numeric_limits<double>::epsilon() * pow(3, 5);
     par.sigma_a = std::numeric_limits<double>::epsilon() * pow(3, 5);
     par.sigma_i = std::numeric_limits<double>::epsilon() * pow(3, 5);
-    
+
     // First run is clean, without shifts. Following are the shift trials.
     for (int ss_i = 0; ss_i < SS_ITER; ss_i++) {
         for (int mb_i = 0; mb_i < MB_ITER; mb_i++) {
@@ -256,20 +260,20 @@ void SemiCrown::trial_coin (Chomp &ch, std::ofstream &log) {
                 for (int i = 0; i < CROWN_SAMPLES; i++) {
                     present_benchmark(ch, seed, body, inertial, focus, MB_MIN + mb_i * MB_STEP, q_actual);
                     Benchmark input(seed, body, focus, WORKING_FOV);
-                    
+
                     // Append our error.
                     input.shift_light((int) input.stars.size(), ((ss_i == 0) ? 0 : SS_MULT * pow(10, ss_i)));
                     double p = ES_MIN + ES_STEP * es_i, clean_size = input.stars.size();
                     input.add_extra_light((int) ((p / (1 - p)) * clean_size));
-                    
+
                     // Find our result set. Run the identification.
                     z = 0;
                     q = Coin::trial_semi_crown(input, par, z);
-                    
+
                     // Log our results.
                     log << "Coin," << par.match_sigma << "," << par.sigma_a << ","
                         << ((ss_i == 0) ? 0 : SS_MULT * pow(10, ss_i)) << "," << MB_MIN + mb_i * MB_STEP << "," << p
-                        << "," << z << "," << Rotation::rotation_difference(q_actual, q, inertial.back()).norm()
+                        << "," << z << "," << Rotation::rotation_difference(q_actual, q, inertial[0]).norm()
                         << "\n";
                 }
             }
