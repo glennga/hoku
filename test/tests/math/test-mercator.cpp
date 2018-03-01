2fa98254ebbe4d42e0ebe32f7569f41c0af52596
Glenn Galvizo <glennjgalvizo@gmail.com>
2018-02-28 17:18:23 -1000

    Now confident in Mercator.
    Figured out correct term for R.

--- test/tests/math/test-mercator.cpp
+++ test/tests/math/test-mercator.cpp
@@ -13,8 +13,7 @@
 #include "math/mercator.h"
 #include "math/random-draw.h"
 
-/// Check that the conversion between 2D and 3D retains the given pixel distance. We are looking for **very**
-/// ballpark estimates.
+/// Check that the conversion between 2D and 3D retains the given pixel distance. We are looking for ballpark estimates.
 TEST(MercatorTransform, Point) {
     for (unsigned int i = 0; i < 20; i++) {
         Vector2 a(RandomDraw::draw_real(-2500, 2500), RandomDraw::draw_real(-2500, 2500));
@@ -24,7 +23,7 @@ TEST(MercatorTransform, Point) {
         Vector3 c = Mercator::transform_point(a.X, a.Y, 5.0 / 5000.0);
         Vector3 d = Mercator::transform_point(b.X, b.Y, 5.0 / 5000.0);
         double theta = (180.0 / M_PI) * Vector3::Angle(c, d);
-        EXPECT_NEAR(theta, distance, 5.0);
+        EXPECT_NEAR(theta, distance, 0.01);
     }
 }
 
