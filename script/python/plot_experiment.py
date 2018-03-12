7ae18a56267d39c8a95519e2049c36f3a13e30a0
Glenn Galvizo <glennjgalvizo@gmail.com>
2018-03-11 21:01:41 -1000

    Experiment visualization script now working as intended.

--- script/python/plot_experiment.py
+++ script/python/plot_experiment.py
@@ -10,10 +10,10 @@ Identification: IdentificationMethod TEXT, Timestamp TEXT, Sigma1 FLOAT, Sigma2 
                     ShiftDeviation FLOAT, CameraSensitivity FLOAT, FalseStars INT, ComparisonCount INT, 
                     PercentageCorrect FLOAT        
 
-The only argument passed to this file is type of experiment to plot. This must be in the space [query, reduction, 
-identification].
+There exists two possible arguments passed to this file: the experiment to plot and a secondary location to the
+lumberjack database relative to HOKU_PROJECT_PATH.
 
-Usage: plot_experiment [experiment-to-visualize]
+Usage: plot_experiment [experiment-to-visualize] [location-of-lumberjack]
 """""
 
 import sqlite3
@@ -32,95 +32,165 @@ def attach_plot_info(att):
     :param att: Dictionary containing attributes, y transformation, and configuration file prefix to plot with.
     :return: None.
     """
-    plt.rc('text', usetex=True), plt.rc('font', family='serif', size=12)
-    sec, pre = att['param_section'], att['params_prefix']
+    sec, pre = att['params_section'], att['params_prefix']
 
     # Set Y limits.
     ax = plt.gca()
     ax.set_ylim(params[sec][pre + '-yll'])
 
-    # Add the bar chart X axis tick labels.
-    x_labels = params[sec][pre + '-xtl']
-    plt.xticks(np.arange(len(x_labels)), x_labels)
+    # If desired, set the X axis as logarithmic.
+    try:
+        if params[sec][pre + '-lxa'] == 1:
+            ax.set_xscale('log')
+    except KeyError:
+        pass
+
+    # Add the legend.
+    leg = plt.legend(params[sec]['ll'], fontsize=18)
+    leg.draggable(True)
+
+    # Add the chart X axis tick labels (if available).
+    try:
+        x_labels = params[sec][pre + '-xtl']
+        plt.xticks(np.arange(len(x_labels)), x_labels)
+    except KeyError:
+        pass
 
     # Add the chart X and Y labels.
     plt.xlabel(params[sec][pre + '-xal']), plt.ylabel(params[sec][pre + '-yal'])
 
 
-def bar_plot(cur_i, att):
-    """ Generic bar plot function. Given a cursor to the results database and a dictionary of attributes, create a bar
-    plot in global 'plt' object. User can choose to move this into a subplot or make this one plot.
+def e_plot(cur_i, att):
+    """ Generic plot function. Given a cursor to the results database and a dictionary of attributes, create a bar or
+    line plot in global 'plt' object. User can choose to move this into a subplot or make this one plot.
 
     :param cur_i: Cursor to database containing result data.
-    :param att: Dictionary containing attributes, y transformation, and params prefix to plot with.
+    :param att: Dictionary containing attributes and params prefix to plot with.
     :return: None.
     """
-    for k, m in enumerate(['Angle', 'Dot', 'Sphere', 'Plane', 'Pyramid', 'Composite']):
+    for k, m in enumerate(params[att['params_section']]['ll']):
         # Grab the possible X-axis values.
-        x_space = cur_i.execute('SELECT DISTINCT ? '
-                                'FROM {}'.format(att['table_name']), (att['x_attribute'],)).fetchall()
+        x_space = cur_i.execute('SELECT DISTINCT {} '.format(att['x_attribute']) +
+                                'FROM {} '.format(att['table_name']) +
+                                'WHERE IdentificationMethod = ? ' +
+                                ('' if 'constrain_that' not in att.keys() else 'AND ' + att['constrain_that']),
+                                (m,)).fetchall()
         x_space = list(map(lambda x: x[0], x_space))
 
         # Construct the points to plot.
-        y_avg, y_std = [[] for _ in x_space], [[] for _ in x_space]
+        y_avg, y_std = [], []
         for x_p in x_space:
-            y_data = cur_i.execute('SELECT ? '  # Grab our Y axis data. 
-                                   'FROM {} '
+            y_data = cur_i.execute('SELECT {} '.format(att['y_attribute']) +  # Grab our Y axis data.
+                                   'FROM {} '.format(att['table_name']) +
                                    'WHERE IdentificationMethod = ? '
-                                   'AND ? = ? ' +
-                                   ('' if 'constrain_that' not in att.keys()
-                                   else 'AND ' + att['constrain_that']).format(att['table_name']),
-                                   (att['y_attribute'], m, att['x_attribute'], x_p)).fetchall()
+                                   'AND {} = ? '.format(att['x_attribute']) +
+                                   ('' if 'constrain_that' not in att.keys() else 'AND ' + att['constrain_that']),
+                                   (m, x_p)).fetchall()
             y_data = list(map(lambda y: y[0], y_data))
 
-            # Divide data into n sections. Apply lambda to each split of y data.
+            # Divide data into n sections. Apply AVG to each split of y data.
             y_data = np.split(np.array(y_data), params['split-n'])
-            y_data = list(map(lambda y: att['y_function'](y), y_data))
+            y_data = list(map(lambda y: np.mean(y), y_data))
 
             # Collect averages and deviations for each partition of data.
-            y_avg.append(np.mean(y_data)[0]), y_std.append(np.std(y_data)[0])
+            y_avg.append(np.mean(y_data)), y_std.append(np.std(y_data))
 
-        # Construct the bar plot and attach plot info.
-        plt.bar(np.arange(len(x_space)) + 0.15 * k - 0.3, y_avg, 0.15, yerr=y_std)
-        attach_plot_info(att)
+        # Construct the appropriate plot and attach plot info.
+        if att['plot_type'] == 'BAR':
+            plt.bar(np.arange(len(x_space)) + 0.15 * k - 0.3, y_avg, 0.15, yerr=y_std)
+            attach_plot_info(att)
+        else:
+            plt.plot(x_space, y_avg)
+            attach_plot_info(att)
+            # plt.errorbar(x_space, y_avg, yerr=y_std)
+
+
+def query_plots(cur_i):
+    """ Display the plots associated with the query experiment.
+
+    :param cur_i: Cursor to database containing result data.
+    :return: None.
+    """
+    plt.rc('text', usetex=True), plt.rc('font', family='serif', size=12)
+
+    plt.figure()
+    plt.subplot(121)  # Sigma1 vs. SExistence visualization.
+    e_plot(cur_i, {'table_name': 'QUERY', 'x_attribute': 'Sigma1', 'y_attribute': 'SExistence',
+                   'constrain_that': '(Sigma2 = Sigma1 OR Sigma2 = 0) '
+                                     'AND (Sigma3 = Sigma1 OR Sigma3 = 0) '
+                                     'AND ShiftDeviation=0 ',
+                   'params_section': 'query-plot', 'params_prefix': 's1se', 'plot_type': 'LINE'})
+
+    plt.subplot(122)  # Sigma1 vs. CandidateSetSize visualization.
+    e_plot(cur_i, {'table_name': 'QUERY', 'x_attribute': 'Sigma1', 'y_attribute': 'CandidateSetSize',
+                   'constrain_that': '(Sigma2 = Sigma1 OR Sigma2 = 0) '
+                                     'AND (Sigma3 = Sigma1 OR Sigma3 = 0) '
+                                     'AND ShiftDeviation=0 ',
+                   'params_section': 'query-plot', 'params_prefix': 's1css', 'plot_type': 'LINE'})
+
+    # plt.figure()
+    # plt.subplot(121)  # ShiftDeviation vs. SExistence visualization.
+    # e_plot(cur_i, {'table_name': 'QUERY', 'x_attribute': 'ShiftDeviation', 'y_attribute': 'SExistence',
+    #                  'params_section': 'query-plot', 'params_prefix': 'sdse', 'plot_type': 'BAR'})
+    #
+    # plt.subplot(122)  # ShiftDeviation vs. CandidateSetSize visualization.
+    # bar_plot(cur_i, {'table_name': 'QUERY', 'x_attribute': 'ShiftDeviation', 'y_attribute': 'CandidateSetSize',
+    #                  'params_section': 'query-plot', 'params_prefix': 'sdcss', 'plot_type': 'BAR'})
+    plt.show()
+
+
+def reduction_plots(cur_i):
+    """ Display the plots associated with the reduction experiment.
+
+    :param cur_i: Cursor to database containing result data.
+    :return: None.
+    """
+    plt.rc('text', usetex=True), plt.rc('font', family='serif', size=12)
+
+    plt.figure()
+    plt.subplot(121)
+    e_plot(cur_i, {'table_name': 'REDUCTION', 'x_attribute': 'ShiftDeviation', 'y_attribute': 'ResultMatchesInput',
+                   'constrain_that': 'CameraSensitivity = 6.0', 'params_section': 'reduction-plot',
+                   'params_prefix': 'sdrmi'})
+
+    plt.subplot(122)
+    e_plot(cur_i, {'table_name': 'REDUCTION', 'x_attribute': 'CameraSensitivity', 'y_attribute': 'ResultMatchesInput',
+                   'constrain_that': 'ShiftDeviation = 1.0e-3', 'params_section': 'reduction-plot',
+                   'params_prefix': 'csrmi'})
+    plt.show()
+
+
+def identification_plots(cur_i):
+    """ Display the plots associated with the identification experiment.
+
+    :param cur_i: Cursor to database containing result data.
+    :return: None.
+    """
+    plt.rc('text', usetex=True), plt.rc('font', family='serif', size=12)
 
 
 if __name__ == '__main__':
     # Ensure that there exists only one argument, and that it is in the appropriate space.
-    if len(sys.argv) != 2 or sys.argv[1] not in ['query', 'reduction', 'identification']:
-        print('Usage: python3 plot_experiment.py [query/reduction/identification]'), exit(1)
+    if len(sys.argv) < 1 or len(sys.argv) > 3 or sys.argv[1] not in ['query', 'reduction', 'identification']:
+        print('Usage: python3 plot_experiment.py [query/reduction/identification] [lumberjack-location]'), exit(1)
 
-    # Experiment data in lumberjack.db.
+    # Experiment data in lumberjack.db, or is specified by the user (second argument).
     conn = ''
     try:
-        conn = sqlite3.connect(os.environ['HOKU_PROJECT_PATH'] + '/CONFIG.ini')
+        if len(sys.argv) == 3 and not os.path.isabs(sys.argv[2]):
+            conn = sqlite3.connect(os.getcwd() + '/' + sys.argv[2])
+        elif len(sys.argv) == 3:
+            conn = sqlite3.connect(sys.argv[2])
+        else:
+            conn = sqlite3.connect(os.environ['HOKU_PROJECT_PATH'] + '/data/lumberjack.db')
     except sqlite3.Error as e:
         print('SQL Error: ' + str(e)), exit(2)
-    cur = conn.cursor(os.environ['HOKU_PROJECT_PATH'] + '/data/lumberjack.db')
+    cur = conn.cursor()
 
+    # Plot the data.
     if sys.argv[1] == 'query':
-        # Run all query visualizations.
-        plt.figure()
-        bar_plot(cur, {table_name: 'QUERY', x_attribute: 'ShiftDeviation', y_attribute: 'SExistence',
-                   y_function: lambda y: np.sum(y), params_section: 'query-plot', params_prefix: 'sdse'})
-        plt.show()
-        plt.figure()
-        bar_plot(cur, {table_name: 'QUERY', x_attribute: 'ShiftDeviation', y_attribute: 'CandidateSetSize',
-                   y_function: lambda y: np.mean(y), params_section: 'query-plot', params_prefix: 'sdcss'})
-        plt.show()
+        query_plots(cur)
     elif sys.argv[1] == 'reduction':
-        # Run all reduction visualizations.
-        bar_plot(cur, {table_name: 'REDUCTION', x_attribute: 'ShiftDeviation', y_attribute: 'ResultMatchesInput',
-                   constrain_that: 'CameraSensitivity = 6.0', y_function: lambda y: np.sum(y),
-                   params_section: 'reduction-plot', params_prefix: 'sdrmi'})
-        bar_plot(cur, {table_name: 'REDUCTION', x_attribute: 'CameraSensitivity', y_attribute: 'ResultMatchesInput',
-                   constrain_that: 'ShiftDeviation = 1.0e-3', y_function: lambda y: np.sum(y),
-                   params_section: 'reduction-plot', params_prefix: 'csrmi'})
+        reduction_plots(cur)
     else:
-        # Run all identification visualizations.
-        bar_plot(cf, cur, 'ShiftDeviation', 'ComparisonCount', 'AVG', 'i-sdcc')
-        bar_plot(cf, cur, 'ShiftDeviation', 'PercentageCorrect', 'AVG', 'i-sspc')
-        bar_plot(cf, cur, 'CameraSensitivity', 'ComparisonCount', 'AVG', 'i-cscc')
-        bar_plot(cf, cur, 'CameraSensitivity', 'PercentageCorrect', 'AVG', 'i-cspc')
-        bar_plot(cf, cur, 'FalseStars', 'ComparisonCount', 'AVG', 'i-fscc')
-        bar_plot(cf, cur, 'FalseStars', 'PercentageCorrect', 'AVG', 'i-fspc')
+        identification_plots(cur)
