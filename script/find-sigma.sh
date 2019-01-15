d617aff9443d2fa3df344bd85e708a722f73fa5c
Glenn <gjgalvizo@gmail.com>
2019-01-14 19:17:36 -1000

    Tested everything but actually running the query.

--- script/find-sigma.sh
+++ script/find-sigma.sh
@@ -13,8 +13,8 @@ if [[ ! -f ${HOKU_PROJECT_PATH}/data/nibble.db ]]; then
 fi
 
 # Ensure that our method is valid.
-if [[ "$1" != "angle" ] && [ "$1" != "dot" ] && [ "$1" != "sphere" ] && [ "$1" != "plane" ] && \
-    [ "$1" != "composite" ]]; then
+if [[ "$1" != "angle" ]] && [[ "$1" != "dot" ]] && [[ "$1" != "sphere" ]] && [[ "$1" != "plane" ]] && \
+   [[ "$1" != "composite" ]]; then
     echo "Method not valid. Use from: [angle / dot / sphere / plane / pyramid / composite]"
     exit 1
 fi
@@ -29,10 +29,10 @@ ROWS=$(sqlite3 ${HOKU_PROJECT_PATH}/data/nibble.db "SELECT COUNT(*) \
                                                     FROM ${TABLE_NAME}_20")
 
 # Determine the first part of our query: the results we want to find.
-if [[ "$1" = "angle" ] || [ "$1" = "pyramid" ]]; then
+if [[ "$1" = "angle" ]] || [[ "$1" = "pyramid" ]]; then
     QUERY_PART_ONE="SELECT MIN(ABS(D_THETA))"
 
-elif [[ "$1" = "sphere" ] || [ "$1" = "plane" ] || [ "$1" = "composite" ]]; then
+elif [[ "$1" = "sphere" ]] || [[ "$1" = "plane" ]] || [[ "$1" = "composite" ]]; then
     QUERY_PART_ONE="SELECT MIN(ABS(D_A)), \
                     MIN(ABS(D_I))"
 
@@ -43,10 +43,10 @@ else
 fi
 
 # Determine the second part of our query: the cartesian product of the table with itself, how do we find our result?
-if [[ "$1" = "angle" ] || [ "$1" = "pyramid" ]]; then
+if [[ "$1" = "angle" ]] || [[ "$1" = "pyramid" ]]; then
     QUERY_PART_TWO="SELECT A.theta - B.theta AS D_THETA"
 
-elif [[ "$1" = "sphere" ] || [ "$1" = "plane" ] || [ "$1" = "composite" ]]; then
+elif [[ "$1" = "sphere" ]] || [[ "$1" = "plane" ]] || [[ "$1" = "composite" ]]; then
     QUERY_PART_TWO="SELECT A.a - B.a AS D_A, \
                     A.i - B.i AS D_I"
 
@@ -57,17 +57,17 @@ else
 fi
 
 # Determine the third part of our query: filter out any zero results we get.
-if [[ "$1" = "angle" ] || [ "$1" = "pyramid" ]]; then
+if [[ "$1" = "angle" ]] || [[ "$1" = "pyramid" ]]; then
     QUERY_PART_THREE="D_THETA > 0"
 
-elif [[ "$1" = "sphere" ] || [ "$1" = "plane" ] || [ "$1" = "composite" ]]; then
+elif [[ "$1" = "sphere" ]] || [[ "$1" = "plane" ]] || [[ "$1" = "composite" ]]; then
     QUERY_PART_THREE="D_A > 0 AND \
-                    D_I > 0"
+                      D_I > 0"
 
 else
     QUERY_PART_THREE="D_THETA_1 > 0 AND \
-                    D_THETA_2 > 0 AND \
-                    D_PHI > 0"
+                      D_THETA_2 > 0 AND \
+                      D_PHI > 0"
 fi
 
 # Divide our query and perform accordingly. We output the results of each query to the temp directory.
@@ -85,7 +85,7 @@ for r in `seq 1 $2`; do
                                                     B.ROWID < $UPPER_QUERY_BOUND AND \
                                                     A.ROWID <> B.ROWID
                                                  )
-                                                 WHERE $QUERY_PART_THREE" >> ${TMP_RESULTS_DIR}/MIN-$1-${r}.result &
+                                                 WHERE $QUERY_PART_THREE;" >> ${TMP_RESULTS_DIR}/MIN-$1-${r}.result &
 done
 
 # Wait for all spawned processes to finish.
